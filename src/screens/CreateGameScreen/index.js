1441f0fc1f440150883ebed1c2b7a09a427c9ea7
Johanna Hermansson <id15jhn@cs.umu.se>
2018-02-28 18:10:31 +0100

    ?

--- src/screens/CreateGameScreen/index.js
+++ src/screens/CreateGameScreen/index.js
@@ -8,19 +8,12 @@ class App extends Component {
   render() {
     return (
       <GridContainer>
-<<<<<<< HEAD
           <img src={logo} className="TestImage" alt="logo"/>
           <img src={logo} className="TestImage" alt="logo"/>
 
           <TableComponent label="hej">
             <p> JOhanaHANSD </p><p> 23948</p>
           </TableComponent>
-=======
-        <p>player 1:1</p>
-        <p>player 1:2</p>
-        <p>player 2:1</p>
-        <p>player 2:2</p>
->>>>>>> 2f8accc1ee75ef3fe8d0013405fd8f029d58a203
       </GridContainer>
     );
   }
