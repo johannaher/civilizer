2dba0e86b33a463c70f0ce4fd8a9a790f9d27f2b
Jesper Blomqvist <jesper.blomqvist@hotmail.com>
2018-02-28 15:03:07 +0100

    creategamescreen
    creategamescreen

--- src/screens/CreateGameScreen/index.js
+++ src/screens/CreateGameScreen/index.js
@@ -1,18 +1,15 @@
-import React, { Component } from 'react';
-import logo from '../../assets/icons/logo.svg';
+import React, { Component } from 'react'
+import logo from '../../assets/icons/logo.svg'
+import GridContainer from '../../components/GridContainer/GridContainer.js'
+import style from './index.css'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started PARTY, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <GridContainer>
+          <img src={logo} className="TestImage" alt="logo"/>
+          <img src={logo} className="TestImage" alt="logo"/>
+      </GridContainer>
     );
   }
 }
