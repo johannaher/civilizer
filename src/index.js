dd3a78d38a397ebe52b2adecd2ea3e3e1353153d
Jesper Blomqvist <jesper.blomqvist@hotmail.com>
2018-02-28 13:27:09 +0100

    added stuff

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import CreateGameScreen from './screens/CreateGameScreen';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<CreateGameScreen />, document.getElementById('root'));
 registerServiceWorker();
