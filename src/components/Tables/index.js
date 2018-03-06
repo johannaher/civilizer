768b4190d979eb84441cd7af1e2f80469c642f65
Johanna Hermansson <id15jhn@cs.umu.se>
2018-03-06 11:47:42 +0100

    tabellll

--- src/components/Tables/index.js
+++ src/components/Tables/index.js
@@ -14,7 +14,7 @@ export default class ButtonComponent extends Component{
     return(
       <table className="civ-table">
         {this.state.civilizationList.map((civ)=>{
-          return <tr> <td id="id">{civ.id}</td> <td id="civilization"> {civ.civilization} </td> <td id="leader">{civ.leader} </td></tr>
+          return <tr> <td id="civilization"> {civ.civilization} </td> <td id="leader">{civ.leader} </td></tr>
         })}
       </table>
     )
