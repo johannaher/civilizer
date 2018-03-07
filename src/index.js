2b1eb5a49534be41832d28311f5e579adabba8d3
Jesper Blomqvist <jesper.blomqvist@hotmail.com>
2018-03-07 09:48:15 +0100

    Added result screen

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './index.css';
 import CreateGameScreen from './screens/CreateGameScreen';
 import HomeScreen from './screens/HomeScreen';
+import Results from './screens/ResultScreen';
 import registerServiceWorker from './registerServiceWorker';
 const App = () => (
     <Router>
       <div>
         <Route exact path = "/" component = {HomeScreen}/>
         <Route path = "/CreateGameScreen" component = {CreateGameScreen}/>
+        <Route path = "/Results" component = {Results}/>
       </div>
     </Router>
   )
---
+++ src/screens/ResultScreen/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import logo from '../../assets/icons/logo.svg'
+import GridContainer from '../../components/GridContainer'
+import style from './index.css'
+import Tables from '../../components/Tables'
+import ButtonComponent from '../../components/Buttons'
+import GamePlayersContainer from '../../components/GamePlayersContainer'
+import Header from '../../components/Headers'
+
+export default class CreateGameScreen extends Component {
+  render() {
+    return (
+        <GridContainer>
+          <p>Results</p>
+        </GridContainer>
+    );
+  }
+}
