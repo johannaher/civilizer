3bace5f3e3dfeb1ed32f21b7d4ab727bd97bb2d2
Johanna Hermansson <id15jhn@cs.umu.se>
2018-03-07 10:37:33 +0100

    :)

--- src/components/Tables/index.js
+++ src/components/Tables/index.js
@@ -19,8 +19,8 @@ export default class TableComponents extends Component{
             <td id="icon"><img class="civ-icon" src="https://d1u5p3l4wpay3k.cloudfront.net/civ6_gamepedia_en/f/f1/Icon_district_hansa.png?version=76007f5cbcad2c226eae842c5ad1300e"/></td>
             <td id="leader">{civ.leader}</td>
             <td id="civilization"> {civ.civilization} </td>
-            <td id="ability"></td>
-            <td>
+            <td id="ability">(founding father ex)</td>
+            <td id="checkbox">
               <label class="checkbox-container">
                   <input type="checkbox"/>
                   <span></span>
