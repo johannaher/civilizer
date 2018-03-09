e9480d5928e9180f9fdd144bb7c05a18756a39db
Jesper Blomqvist <jesper.blomqvist@hotmail.com>
2018-03-09 12:02:42 +0100

    Made playercontainer use actual players

--- src/components/PlayerContainer/index.js
+++ src/components/PlayerContainer/index.js
@@ -2,11 +2,18 @@ import React, { Component } from 'react'
 import style from './index.css'
 
 export default class PlayerContainer extends Component {
+
   render() {
     return (
       <div className = "player-container">
 
-        <p>{this.props.children}</p>
+        <p><img className = "user-img" src={this.props.user.imgUrl}/>
+        <p>{this.props.user.name}</p></p>
+
+        <label class="checkbox-container">
+            <input type="checkbox"/>
+            <span></span>
+        </label>
 
       </div>
 
--- src/components/Tables/index.js
+++ src/components/Tables/index.js
@@ -1,20 +1,14 @@
 import React, { Component } from 'react';
 import './index.css';
-import civilizationList from '../Mockobject';
 
 export default class TableComponents extends Component{
-  constructor(props){
-    super(props)
-    this.state = {
-      civilizationList: civilizationList
-    }
-  }
+
   render(){
     console.log(this.state)
     return(
 
       <table className="civ-table">
-        {this.state.civilizationList.map((civ)=>{
+        {this.props.civilizationList.map((civ)=>{
           return <tr>
             <td id="icon"><img class="civ-icon" src="https://d1u5p3l4wpay3k.cloudfront.net/civ6_gamepedia_en/f/f1/Icon_district_hansa.png?version=76007f5cbcad2c226eae842c5ad1300e"/></td>
             <td id="leader">{civ.leader}</td>
