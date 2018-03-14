85e12052a4b4aeff65984335f99fb8760a8dfab5
Jesper Blomqvist <jesper.blomqvist@hotmail.com>
2018-03-14 10:01:48 +0100

    removed weird symbols from Johanna

--- src/components/Tables/index.js
+++ src/components/Tables/index.js
@@ -17,7 +17,6 @@ export default class TableComponents extends Component{
 
       <table className="civ-table">
         {this.props.civilizationList.map((civ)=>{
-<<<<<<< HEAD
           return <tr>
             <td id="icon"><img class="civ-icon" src="civ.leaderImg"/></td>
             <td id="leader">{civ.leader}</td>
@@ -30,11 +29,9 @@ export default class TableComponents extends Component{
               </label>
             </td>
             </tr>
-=======
           return(
             <CivilizationContainer civ = {civ}/>
           )
->>>>>>> 09412d05ccd9461d34fada8d1a18a8536591d9c8
         })}
       </table>
     );
