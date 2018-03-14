925ddf6cb350a7394d120ed683ad241484bb59b4
Jesper Blomqvist <jesper.blomqvist@hotmail.com>
2018-03-14 10:06:56 +0100

    Removed tables ugly mock civ stuff

--- src/components/Tables/index.js
+++ src/components/Tables/index.js
@@ -15,18 +15,6 @@ export default class TableComponents extends Component{
     return(
       <table className="civ-table">
         {this.props.civilizationList.map((civ)=>{
-          return <tr>
-            <td id="icon"><img class="civ-icon" src="civ.leaderImg"/></td>
-            <td id="leader">{civ.leader}</td>
-            <td id="civilization"> {civ.civilization} </td>
-            <td id="ability"> {civ.iconImg}</td>
-            <td id="checkbox">
-              <label class="checkbox-container">
-                  <input type="checkbox"/>
-                  <span></span>
-              </label>
-            </td>
-            </tr>
           return(
             <CivilizationContainer civ = {civ}/>
           )
