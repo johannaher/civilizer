24276562834f852e9a4d678df8fc1cd3ed6ca486
Sofia <s_tovedal@hotmail.com>
2018-03-14 12:14:03 +0100

    mock randomizing

--- src/screens/ResultScreen/index.js
+++ src/screens/ResultScreen/index.js
@@ -7,23 +7,50 @@ import GamePlayersContainer from '../../components/GamePlayersContainer'
 import Header from '../../components/Headers'
 import { Redirect } from 'react-router-dom'
 import { civilizations } from '../../api'
+import civList from './mocks/civs.js'
+import playerList from './mocks/players.js'
+import { shuffle } from './utilities'
+import Choice from './components/Choice'
 
-export default class CreateGameScreen extends Component {
+export default class ResultsScreen extends Component {
 
   constructor(props){
     super(props)
     this.state = {
-      civilizedPlayersList: [],
+      users: playerList,
+      civilizations: civList,
+      results: [],
     }
   }
 
+  randomize(){
+    let shuffledCivs = shuffle(this.state.civilizations)
+    let nrOfChoices = Math.floor(this.state.civilizations.length/this.state.users.length)
+    let index = 0;
+    this.state.users.map((user)=>{
+      user.choices = []
+      for( let i = 0 ; i < nrOfChoices ; i++ ){
+        user.choices.push(shuffledCivs[index])
+        index++
+      }
+      this.state.results.push(user)
+    })
+  }
+
+  componentWillMount(){
+    this.randomize()
+  }
+
+
   render() {
-    if(!sessionStorage.isLoggedIn){
+    if(sessionStorage.isLoggedIn==='false'){
       return <Redirect to='/LoginScreen'/>
     }
     return (
       <GridContainer>
-        <p>Hej</p>
+        {this.state.results.map((result) => {
+          return <Choice {...result} />
+        })}
       </GridContainer>
     );
   }
